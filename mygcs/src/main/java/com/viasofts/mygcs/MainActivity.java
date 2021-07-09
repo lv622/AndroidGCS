8dcc372283ec187323a5bd84e2463e95abaa9fea
lv622 <hrkim5620@naver.com>
2021-07-09 12:24:14 +0900

    navermap

--- mygcs/src/main/java/com/viasofts/mygcs/MainActivity.java
+++ mygcs/src/main/java/com/viasofts/mygcs/MainActivity.java
@@ -1,10 +1,12 @@
 package com.viasofts.mygcs;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.graphics.SurfaceTexture;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,10 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 import com.o3dr.android.client.ControlTower;
 import com.o3dr.android.client.Drone;
 import com.o3dr.android.client.interfaces.DroneListener;
@@ -29,7 +35,7 @@ import com.o3dr.services.android.lib.drone.companion.solo.SoloState;
 import com.o3dr.services.android.lib.drone.property.Type;
 import com.o3dr.services.android.lib.gcs.link.LinkConnectionStatus;
 
-public class MainActivity extends AppCompatActivity implements DroneListener, TowerListener, LinkListener {
+public class MainActivity extends AppCompatActivity implements DroneListener, TowerListener, LinkListener, OnMapReadyCallback {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -48,11 +54,25 @@ public class MainActivity extends AppCompatActivity implements DroneListener, To
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        //가로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
 
         final Context context = getApplicationContext();
         this.controlTower = new ControlTower(context);
         this.drone = new Drone(context);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
 
 /*
         this.modeSelector = (Spinner) findViewById(R.id.modeSelect);
@@ -69,6 +89,11 @@ public class MainActivity extends AppCompatActivity implements DroneListener, To
         });
  */
         mainHandler = new Handler(getApplicationContext().getMainLooper());
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setMapType(NaverMap.MapType.Hybrid);
     }
 
     @Override
@@ -193,6 +218,4 @@ public class MainActivity extends AppCompatActivity implements DroneListener, To
     private void runOnMainThread(Runnable runnable) {
         mainHandler.post(runnable);
     }
-
-
 }
